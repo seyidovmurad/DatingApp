a7fdb9b8327cd1137f5dc0a91bd523ffbce561f8
seyidovmurad <smurad218@gmail.com>
2023-02-21 23:20:10 +0400

    First

---
+++ Api/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+
+//using System.ComponentModel.DataAnnotations;
+
+namespace Api.Entities
+{
+    public class AppUser
+    {
+        //[Key]  //this is id
+        public int Id { get; set; }
+
+        public string UserName { get; set; }        
+
+        
+    }
+}
