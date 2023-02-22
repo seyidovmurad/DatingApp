869940c8ed773a54f49fdffb0e0a9901f81e98e0
seyidovmurad <smurad218@gmail.com>
2023-02-22 15:24:05 +0400

    Added Angular

--- Api/Program.cs
+++ Api/Program.cs
@@ -10,10 +10,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(cors => cors.AllowAnyHeader().AllowAnyOrigin().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
